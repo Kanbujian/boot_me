a8dd95ef06440040583ae6747146192a246f9800
kanbujian <462377353@qq.com>
2018-04-17 16:51:22 +0800

    Feat: spring jpa integration

--- src/main/java/com/kanbujian/ApplicationRunner.java
+++ src/main/java/com/kanbujian/ApplicationRunner.java
@@ -3,12 +3,16 @@ package com.kanbujian;
 import com.kanbujian.controller.MessageController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan
+@EnableJpaRepositories("com.kanbujian.dao")
+@EntityScan("com.kanbujian.entity")
+@ComponentScan("com.kanbujian")
 public class ApplicationRunner {
     public static void main(String[] args){
         SpringApplication.run(ApplicationRunner.class, args);
--- src/main/java/com/kanbujian/controller/TransactionsController.java
+++ src/main/java/com/kanbujian/controller/TransactionsController.java
@@ -1,8 +1,10 @@
 package com.kanbujian.controller;
 
-import com.kanbujian.dao.impl.TransactionDaoImpl;
+import com.kanbujian.dao.TransactionDao;
 import com.kanbujian.entity.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
@@ -13,27 +15,33 @@ import java.util.Map;
 @RequestMapping("transactions")
 public class TransactionsController {
     @Autowired
-    private TransactionDaoImpl transactionDao;
+    private TransactionDao transactionDao;
 
     @GetMapping()
-    public @ResponseBody List index(){
-        List<Transaction> transactionList= transactionDao.findAll();
+    public @ResponseBody List index(@RequestBody Map params){
         System.out.println("hahah");
+        Pageable pageable = new PageRequest((Integer) params.get("page"), (Integer)params.get("per"));
+        List<Transaction> transactionList = (List<Transaction>) transactionDao.findAll(pageable);
         return transactionList;
     }
 
     @PostMapping()
     @Transactional
     public @ResponseBody Map create(){
-        Transaction transaction = new Transaction("cny", 500, "chare");
+        Transaction transaction = new Transaction("cny", 1000, "refund");
         transactionDao.save(transaction);
         System.out.println("save successful");
         return null;
     }
 
+    @GetMapping("/{id}")
+    public @ResponseBody Transaction show(){
+        Transaction transaction = transactionDao.findFirstByCurrency("cny");
+        return transaction;
+    }
+
     @PutMapping("/{id}")
     public @ResponseBody Map update(){
-
         return null;
     }
 
